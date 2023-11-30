411de9de2e8a248fcb208eb72186d196f6229f2b
Alexanderel04 <2004.estrada.lopez@gmail.com>
2023-11-30 10:15:13 -0600

    "Commit by Alejandro Estrada 30/11/2023"

--- views/admin/addBooks.php
+++ views/admin/addBooks.php
@@ -2,7 +2,7 @@
     <div class="w-full md:w-2/3" >
     <div class="text-center" >
         <h1 class="font-bold text-3xl text-green-600" >Administrador de Libros</h1>
-        <p class="text-xl font-bold" >Agregar los datos que se piden en el formulario para agregar eun libro</p>
+        <p class="text-xl font-bold" >Agregar los datos que se piden en el formulario para agregar un libro</p>
     </div>
     <form class="flex flex-col items-center bg-slate-100 shadow-lg p-5 rounded-md mt-5 gap-3" action="/add-books" method="POST" >
         <div class="flex flex-col gap-2 w-full" >
